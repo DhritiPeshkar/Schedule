c2998ac2f56406e7fe8d4e2283fce18608c318d7
Dhriti Peshkar <dhritipesh@gmail.com>
2025-08-30 11:24:33 -0700

    Initial Commit

---
+++ ScheduleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScheduleApp
+//
+//  Created by Dhriti Peshkar on 8/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScheduleApp/ScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleAppApp.swift
+//  ScheduleApp
+//
+//  Created by Dhriti Peshkar on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
